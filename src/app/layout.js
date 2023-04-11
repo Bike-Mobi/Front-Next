fd5ed080c18e3d31ab4943f099a67d84849244ed
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-04-11 09:13:24 -0300

    :sparkles: feat: add fonts links

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt">
+      <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"/>
+        {/* font-family: 'DM Sans', sans-serif;
+        font-family: 'Roboto', sans-serif; */}
+      </head>
       <body>{children}</body>
     </html>
   )
