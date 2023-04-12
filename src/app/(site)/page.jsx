96ffb1485f996f7d4065bede43ed646e4fe35089
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-04-12 10:54:59 -0300

    :sparkles: feat: adiciona section na landing page

--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -1,11 +1,17 @@
 import AboutUs from "@/components/site/AboutUs";
+import Author from "@/components/site/Author";
+import Feedback from "@/components/site/Feedback";
+import Footer from "@/components/site/Footer";
 import Hero from "@/components/site/Hero";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <AboutUs/>
+      <AboutUs />
+      <Feedback />
+      <Author />
+      <Footer/>
     </main>
   )
 }
