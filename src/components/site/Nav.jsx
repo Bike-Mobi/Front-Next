0aad9bd2a6bda34da24984632926339014671ac1
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-04-12 10:52:49 -0300

    :sparkles: feat: fixed nav

--- src/components/site/Nav.jsx
+++ src/components/site/Nav.jsx
@@ -16,7 +16,7 @@ const Nav = () => {
     nav ? transition = 'left-0' :  transition = 'left-[-100%]'
 
     return (
-        <div className='flex font-dmsans'>
+        <div className='flex font-dmsans fixed'>
             <div className='bg-white h-20 w-screen shadow-lg flex justify-end '>
                 <Link className='md:block hidden text-tomEscuro font-bold px-8 py-2 h-fit my-auto mr-14' href='/classificados'>
                     Classificados
@@ -24,7 +24,7 @@ const Nav = () => {
                 <Link className='md:block hidden bg-azul text-white font-bold rounded-lg px-8 py-2 h-fit my-auto mr-24' href='/login'>
                     Login
                 </Link>
-                <div className={`absolute top-0 bg-azul w-[60%] h-full border-r border-white text-white font-semibold ease-in-out duration-500 ${transition} z-40 flex flex-col`}>
+                <div className={`fixed top-0 bg-azul w-[60%] h-full border-r border-white text-white font-semibold ease-in-out duration-500 ${transition} z-40 flex flex-col`}>
                     <div className='flex justify-center pt-4 pb-14 border-b border-white'>Bike Mobi</div>
                     <Link className='p-4 border-b border-white' href='/classificados'>Classificados</Link>
                     <Link className='p-4 border-b border-white' href='/login'>Login</Link>
