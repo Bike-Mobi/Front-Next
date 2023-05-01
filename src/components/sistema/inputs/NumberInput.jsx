59e7802bd6ed41bf821fa1bef149584359bbf871
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-05-01 18:51:33 -0300

    :construction: build number input component

---
+++ src/components/sistema/inputs/NumberInput.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactInputMask from 'react-input-mask'
+
+const NumberInput = (props) => {
+
+    let req, classReq
+    if(props.required) {
+        req = true
+        classReq = "after:content-['*'] after:ml-0.5 after:text-error"
+    } else {
+        req = false
+    }
+
+    return (
+        <div className={`${props.ClassName} ${props.width}`}>
+            <label className="label">
+                <span className={`label-text font-medium ${classReq}`}>{props.name}</span>
+            </label>
+            <ReactInputMask type="number"
+                required={req}
+                placeholder={props.placeholder}
+                value={props.value}
+                onChange={props.onChange}
+                mask={props.mask}
+                className={`input input-accent input-bordered ${props.width}`}
+            />
+        </div>
+    )
+}
+
+export default NumberInput
