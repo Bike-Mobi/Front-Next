3525cdfd28cdde20b81387946f38a93c24e66541
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-04-30 23:35:29 -0300

    :construction: build: config daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,23 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#3AA0FF",
+          "secondary": "#170F49",
+          "accent": "#979797",
+          "neutral": "#1E1E1E",
+          "base-100": "#fff",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 }
 
