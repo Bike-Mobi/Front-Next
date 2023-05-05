0afcf6e8b033eea646b9a7548cda2467efcc6911
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-05-05 00:31:21 -0300

    :sparkles: feat: add providers

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
+import { AuthProvider } from '@/contexts/Auth'
 import './globals.css'
+import { ApiProvider } from '@/contexts/Api'
 
 export const metadata = {
   title: 'Bike Mobi',
@@ -7,15 +9,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt">
-      <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-      <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"/>
-        {/* font-family: 'DM Sans', sans-serif;
-        font-family: 'Roboto', sans-serif; */}
-      </head>
-      <body>{children}</body>
-    </html>
+    <ApiProvider>
+      <AuthProvider>
+        <html lang="pt">
+          <head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+          <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"/>
+            {/* font-family: 'DM Sans', sans-serif;
+            font-family: 'Roboto', sans-serif; */}
+          </head>
+          <body>{children}</body>
+        </html>
+      </AuthProvider>
+    </ApiProvider>
   )
 }
