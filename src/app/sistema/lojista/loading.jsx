4aecd29067e6574db2be9eabd8e09b69cb39cded
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-05-05 20:59:04 -0300

    :saprkles: feat: add loading

---
+++ src/app/sistema/lojista/loading.jsx
@@ -0,0 +1,11 @@
+import LoadingComponent from '@/components/sistema/loadingComponent'
+import Image from 'next/image'
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <LoadingComponent/>
+    )
+}
+
+export default Loading
