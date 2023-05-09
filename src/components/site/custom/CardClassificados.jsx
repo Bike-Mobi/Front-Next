94b560df045d559575dcae83676518514305893d
PedroBritol <pb_lucasbol@hotmail.com>
2023-05-09 13:32:10 -0300

    :sparkles: feat: botao de modal e corecoes

--- src/components/site/custom/CardClassificados.jsx
+++ src/components/site/custom/CardClassificados.jsx
@@ -1,14 +1,21 @@
 import ModalCardClassificados from './ModalCardClassificados'
-
+import { usePathname } from "next/navigation";
+import { TrashIcon, PencilSquareIcon } from '@heroicons/react/24/outline';
 
 const CardClassificados = (props) => {
 
+    const path = usePathname()
+
     return (
-        <div className='border border-cinza rounded-2xl font-dmsans w-[250px] md:w-[450px] xl:w-[550px] h-[450px] md:h-[220px] p-3 flex-col flex md:flex-row justify-between mb-6'>
-            <div className='flex w-56 md:w-[176px] xl:w-56'>
-                <img src={props.photo} alt="Foto da mercadoria" className=' rounded-2xl object-cover'/>
+        <div className={`border border-cinza rounded-2xl font-dmsans ${ path == "/classificados" ? "md:w-[450px] xl:w-[550px] h-[450px] md:h-[220px] md:flex-row" : ""} w-[250px] h-[450px] flex flex-col p-3 justify-between mb-6`}>
+            <div className={`${ path == "/classificados" ? "hidden" : "block"}`}>
+                <button className='hover:bg-neutral-300 p-1 hover:rounded-full'><TrashIcon className='w-6 h-6 text-[#ff0000]'/></button>
+                <button className='hover:bg-neutral-300 p-1 hover:rounded-full ml-1'><PencilSquareIcon className='w-6 h-6 text-[#008800]'/></button>
             </div>
-            <div className='flex flex-col justify-between w-56 md:w-[240px] xl:w-72 h-full'>
+            <div className={`flex w-56 justify-center ${ path == "/classificados" ? " md:w-[176px] xl:w-56" : ""}`}>
+                <img src={props.photo} alt="Foto da mercadoria" className='rounded-2xl object-cover h-[195px]'/>
+            </div>
+            <div className={`flex flex-col justify-between w-56 h-full ${path == "/classificados" ? "md:w-[240px] xl:w-72" : ""}`}>
                 <div className='flex justify-center font-bold text-tomEscuro text-lg max-h-14 break-all overflow-hidden'>
                     {props.title}
                 </div>
@@ -16,8 +23,15 @@ const CardClassificados = (props) => {
                     {props.description}
                 </div>
                 <div className='flex text-base md:text-lg justify-between items-center pr-3'>
-                    <span className='text-sm xl:text-base max-w-[140px] break-words overflow-hidden font-bold text-tomEscuro'>Valor: {props.price}</span>
-                    <button className='btn btn-primary text-white btn-xs xl:btn-sm'>Detalhes</button>
+                    <span className='text-sm xl:text-base break-words overflow-hidden font-bold text-tomEscuro max-h-11'>Valor: {props.price}</span>
+
+                    <ModalCardClassificados
+                        title={props.title}
+                        photo={props.photo}
+                        description={props.description}
+                        price={props.price}
+                        id={props.id}
+                    ></ModalCardClassificados>
                 </div>
             </div>
         </div>
