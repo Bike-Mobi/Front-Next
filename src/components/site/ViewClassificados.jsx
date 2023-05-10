760a8f35e12f17eea0e19466079aefa92674e418
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-05-10 18:22:51 -0300

    :goal_net: fix: styles view classificados

--- src/components/site/ViewClassificados.jsx
+++ src/components/site/ViewClassificados.jsx
@@ -12,7 +12,7 @@ const ViewClassificados = (props) => {
 
     const path = usePathname()
 
-    const produtosFiltrados = props.produtos.filter((item) => {
+    const produtosFiltrados = props.produtos?.filter((item) => {
         return item.title.toLowerCase().includes(search.toLowerCase())
     })
 
@@ -21,15 +21,15 @@ const ViewClassificados = (props) => {
         <div>
             <div className="bg-azul rounded-xl p-4 md:py-8 lg:p-8 my-16 mx-8 md:mx-32 flex flex-col md:flex-row">
                 <div className="flex mb-3 md:mb-0 w-full items-center relative">
-                    <input onChange={(e) => setSearch(e.target.value)} placeholder="Pesquisar um anúncio" type="text" name="input-search" id="input-search" className="border p-2 pl-3 rounded-md border-white bg-azul placeholder:text-white text-white w-full" />
-                    <MagnifyingGlassIcon className="w-6 h-6 absolute right-2"/>
+                    <input onChange={(e) => setSearch(e.target.value)} placeholder="Pesquisar um anúncio" type="text" name="input-search" id="input-search" className="border p-2 pl-3 rounded-md border-white bg-azul placeholder:text-neutral-300 text-white w-full focus:outline-none" />
+                    <MagnifyingGlassIcon className="w-6 h-6 absolute right-2 text-white"/>
                 </div>
-                <button className="bg-tomEscuro text-white lg:px-4 rounded-md font-medium flex items-center justify-center py-1 md:py-0 w-[190px] md:w-[257px] lg:w-[293px] text-sm lg:text-base md:ml-4" ><Link href={ path == "/classificados" ? "/login" : ''}>+ Adicionar um Anúncio</Link></button>
+                <button className="bg-tomEscuro text-white lg:px-4 rounded-md font-medium flex items-center justify-center py-1 md:py-0 w-[190px] md:w-[257px] lg:w-[293px] text-sm lg:text-base md:ml-4" ><Link href={ path == "/classificados" ? "/autenticacao/login" : ''}>+ Adicionar um Anúncio</Link></button>
             </div>
 
             <div className={`grid justify-items-center ${ path == "/classificados" ? "grid-cols-1 lg:grid-cols-2 gap-4" : 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'}`}>
                 {
-                    produtosFiltrados.map((item) => (
+                    produtosFiltrados?.map((item) => (
                         <CardClassificados
                             key={item.id}
                             id={item.id}
