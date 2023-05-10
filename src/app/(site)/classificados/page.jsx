2a7684723ae7ed1ff91be9263e504ce309016f61
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-05-10 18:23:14 -0300

    :construction: build: add classificados itens fake

--- src/app/(site)/classificados/page.jsx
+++ src/app/(site)/classificados/page.jsx
@@ -1,11 +1,29 @@
 'use client'
+import ViewClassificados from '@/components/site/ViewClassificados'
 import React from 'react'
 
 const Classificados = () => {
-    return (
-        <div className='mt-20'>Classificados
-            <div className='font-robot'>Classificados</div>
 
+    const produtos = [
+        {
+            id: 1,
+            title: 'Bike Audax ADX 300',
+            price: '8.299,00',
+            description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco colmer consequat aute irure sint amet.',
+            photo: 'https://semexe.com/blog/wp-content/uploads/2022/03/Scalpel-personalizada-por-Black-tiger-74.jpg'
+        },
+        {
+            id: 2,
+            title: 'Bike OGGI Hacker tamanho 17',
+            price: '6.159,00',
+            description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco colmer consequat aute irure sint amet.',
+            photo: 'https://www.flashbike.com.br/site/carrega?_tp=img5&_img=008326001.jpg'
+        }
+    ]
+
+    return (
+        <div className='pt-20'>
+            <ViewClassificados produtos={ produtos } />
         </div>
     )
 }
