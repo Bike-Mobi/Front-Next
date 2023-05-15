749cac3db452ba65d7a6b0ed5a5bf8e2ff4f3a74
PedroBritol <pb_lucasbol@hotmail.com>
2023-05-15 14:21:20 -0300

    :lipstick: style: classificados

--- src/components/sistema/screens/ClassificadosScreen.jsx
+++ src/components/sistema/screens/ClassificadosScreen.jsx
@@ -1,1 +1,65 @@
-// A view de classificados deve estar aqui!
+'use client'
+import React, { useState } from "react";
+import CardClassificados from '@/components/site/custom/CardClassificados'
+import Link from 'next/link';
+import { usePathname, useRouter } from "next/navigation";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import ModalComponent from "../Modals/ModalComponent";
+
+
+const ClassificadosScreen = (props) => {
+
+    const [search, setSearch] = useState('')
+
+    const path = usePathname()
+    const router = useRouter()
+
+    const produtosFiltrados = props.produtos?.filter((item) => {
+        return item.title.toLowerCase().includes(search.toLowerCase())
+    })
+
+    function renderCards() {
+        if (produtosFiltrados.length > 0) {
+            return produtosFiltrados.map((item) => {
+                return (
+                    <CardClassificados
+                        key={item.id}
+                        title={item.title}
+                        photo={item.photo}
+                        description={item.description}
+                        price={item.price}
+                        id={item.id}
+                    ></CardClassificados>
+                )
+            })
+        } else {
+            return (
+                <div className="flex justify-center items-center flex-col" >
+                    <p className="font-dmsans font-bold text-2xl text-tomEscuro">Nenhum anúncio encontrado</p>
+                    <p className="font-dmsans font-normal text-lg text-tomEscuro">Tente novamente com outra palavra-chave</p>
+                </div>
+            )
+        }
+    }
+
+    return(
+        <div id="teste" className=" ">
+            <div className="bg-azul rounded-xl p-4 lg:py-8 xl:p-8 my-16 mx-8 lg:mx-32 flex flex-col lg:flex-row">
+                <div className="flex mb-3 lg:mb-0 w-full items-center relative">
+                    <input onChange={(e) => setSearch(e.target.value)} placeholder="Pesquisar um anúncio" type="text" name="input-search" id="input-search" className="border p-2 pl-3 rounded-md border-white bg-azul placeholder:text-neutral-300 text-white w-full focus:outline-none" />
+                    <MagnifyingGlassIcon className="w-6 h-6 absolute right-2 text-white"/>
+                </div>
+                <ModalComponent path={path} action="create" id="null"></ModalComponent>
+            </div>
+
+            <div className={`grid justify-items-center ${ path == "/classificados" ? "grid-cols-1 lg:grid-cols-2 gap-4" : 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'}`}>
+                {renderCards()}
+            </div>
+
+        </div>
+    )
+}
+
+
+export default ClassificadosScreen
+
--- src/components/site/custom/CardClassificados.jsx
+++ src/components/site/custom/CardClassificados.jsx
@@ -1,6 +1,6 @@
 import ModalCardClassificados from './ModalCardClassificados'
 import { usePathname } from "next/navigation";
-import { TrashIcon, PencilSquareIcon } from '@heroicons/react/24/outline';
+import ModalComponent from '@/components/sistema/Modals/ModalComponent';
 
 const CardClassificados = (props) => {
 
@@ -8,21 +8,21 @@ const CardClassificados = (props) => {
 
     return (
         <div className={`border border-cinza rounded-2xl font-dmsans ${ path == "/classificados" ? "md:w-[450px] xl:w-[550px] h-[450px] md:h-[220px] md:flex-row" : ""} w-[250px] h-[450px] flex flex-col p-3 justify-between mb-6`}>
-            <div className={`${ path == "/classificados" ? "hidden" : "block"}`}>
-                <button className='hover:bg-neutral-300 p-1 hover:rounded-full'><TrashIcon className='w-6 h-6 text-[#ff0000]'/></button>
-                <button className='hover:bg-neutral-300 p-1 hover:rounded-full ml-1'><PencilSquareIcon className='w-6 h-6 text-[#008800]'/></button>
+            <div className={`${ path == "/classificados" || path.slice(-9) == "/anuncios"  ? "hidden" : "block"} flex gap-2 mb-2`}>
+                <ModalComponent action="delete" id={`${props.id}d`}/>
+                <ModalComponent action="edit" id={`${props.id}e`}/>
             </div>
             <div className={`flex w-56 justify-center ${ path == "/classificados" ? " md:w-[176px] xl:w-56" : ""}`}>
                 <img src={props.photo} alt="Foto da mercadoria" className='rounded-2xl object-cover h-[195px]'/>
             </div>
-            <div className={`flex flex-col justify-between w-56 h-full ${path == "/classificados" ? "md:w-[240px] xl:w-72" : ""}`}>
+            <div className={`flex flex-col justify-evenly  w-56 h-full ${path == "/classificados" ? "md:w-[240px] xl:w-72 md:justify-between" : ""}`}>
                 <div className='flex justify-center font-bold text-tomEscuro text-lg max-h-14 break-all overflow-hidden'>
                     {props.title}
                 </div>
-                <div className='font-normal text-cinza text-xs xl:text-sm break-all overflow-hidden max-h-[109px] xl:max-h-[100px]'>
+                <div className='font-normal text-cinza text-xs xl:text-sm break-all overflow-hidden max-h-[112px] xl:max-h-[100px]'>
                     {props.description}
                 </div>
-                <div className='flex text-base md:text-lg justify-between items-center pr-3'>
+                <div className='flex text-base md:text-lg items-center justify-between'>
                     <span className='text-sm xl:text-base break-words overflow-hidden font-bold text-tomEscuro max-h-11'>Valor: {props.price}</span>
 
                     <ModalCardClassificados
