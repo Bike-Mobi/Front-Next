29a2a1920d516701b7fa42b2c67e95ac9c1fd179
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-05-19 16:01:05 -0300

    :bug: fix: ajuste params dropfile

--- src/components/sistema/inputs/FileInput.jsx
+++ src/components/sistema/inputs/FileInput.jsx
@@ -26,16 +26,16 @@ const FileInput = (props) => {
 
     if (file || props.defaultValue) {
         bg = (
-            <label for="dropzone-file" className="flex flex-col items-center w-full max-w-lg mx-auto text-center bg-white border-2 border-cinza border-dashed cursor-pointer dark:bg-gray-900 dark:border-gray-700 rounded-xl">
+            <label htmlFor="dropzone-file" className="flex flex-col items-center w-full max-w-lg mx-auto text-center bg-white border-2 border-cinza border-dashed cursor-pointer dark:bg-gray-900 dark:border-gray-700 rounded-xl">
                 <img src={props.defaultValue ? props.defaultValue : file} alt="" className=' object-cover rounded-xl'/>
                 <input id="dropzone-file" onChange={getFile} type="file" className="hidden" />
             </label>
         )
     } else {
         bg = (
-            <label for="dropzone-file" className="flex flex-col items-center w-full max-w-lg p-5 mx-auto text-center bg-white border-2 border-cinza border-dashed cursor-pointer dark:bg-gray-900 dark:border-gray-700 rounded-xl">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 h-8 text-cinza dark:text-gray-400">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+            <label htmlFor="dropzone-file" className="flex flex-col items-center w-full max-w-lg p-5 mx-auto text-center bg-white border-2 border-cinza border-dashed cursor-pointer dark:bg-gray-900 dark:border-gray-700 rounded-xl">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8 text-cinza dark:text-gray-400">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                 </svg>
 
                 <h2 className="mt-1 font-medium tracking-wide text-cinza dark:text-gray-200">{props.text}</h2>
