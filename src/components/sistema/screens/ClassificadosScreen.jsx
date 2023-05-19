b7630cad8916c7cd2c77e12e8ddfe6e28fce6647
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-05-18 22:49:22 -0300

    :bug: fix: name modal classificados call

--- src/components/sistema/screens/ClassificadosScreen.jsx
+++ src/components/sistema/screens/ClassificadosScreen.jsx
@@ -4,7 +4,7 @@ import CardClassificados from '@/components/site/custom/CardClassificados'
 import Link from 'next/link';
 import { usePathname, useRouter } from "next/navigation";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import ModalComponent from "../modals/ModalClassificados";
+import ModalClassificados from "../modals/ModalClassificados";
 
 
 const ClassificadosScreen = (props) => {
@@ -49,7 +49,7 @@ const ClassificadosScreen = (props) => {
                     <input onChange={(e) => setSearch(e.target.value)} placeholder="Pesquisar um anúncio" type="text" name="input-search" id="input-search" className="border p-2 pl-3 rounded-md border-white bg-azul placeholder:text-neutral-300 text-white w-full focus:outline-none" />
                     <MagnifyingGlassIcon className="w-6 h-6 absolute right-2 text-white"/>
                 </div>
-                <ModalComponent path={path} action="create" id="null"></ModalComponent>
+                <ModalClassificados path={path} action="create" id="null"></ModalClassificados>
             </div>
 
             <div className={`grid justify-items-center ${ path == "/classificados" ? "grid-cols-1 lg:grid-cols-2 gap-4" : 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'}`}>
