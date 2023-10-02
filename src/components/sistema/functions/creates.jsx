740501bfd7248efcf2dade38c6b68ef730bd6ed2
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-10-02 09:28:33 -0300

    :construction: fire: comments some functions

--- src/components/sistema/functions/creates.jsx
+++ src/components/sistema/functions/creates.jsx
@@ -1,21 +1,21 @@
-import { ApiContext } from '@/contexts/Api'
-import axios from 'axios'
-import { useContext } from 'react'
+// import { ApiContext } from '@/contexts/Api'
+// import axios from 'axios'
+// import { useContext } from 'react'
 
-export default function creates(data, url) {
+// export default function creates(data, url) {
     
-    const {instance} = useContext(ApiContext)
+//     const {instance} = useContext(ApiContext)
 
-    console.log(data)
+//     console.log(data)
 
-    instance.post(`${url}`,
-        data
-    )
+//     instance.post(`${url}`,
+//         data
+//     )
     
-    // setTimeout(() => {
-    //     document.location.reload()
-    // }, 800);
+//     // setTimeout(() => {
+//     //     document.location.reload()
+//     // }, 800);
 
-    return 0
+//     return 0
 
-}
+// }
