7450b43d46db5ac4a4ae1579d42509102630f1e4
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-10-02 09:35:53 -0300

    :sparkles: feat: some layouts

---
+++ src/app/sistema/lojista/bikes/layout.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+const PerfilLayout = (props) => {
+    return (
+        <div>
+            <div className='font-dmsans text-xl text-tomEscuro font-medium absolute left-5 md:left-[280px] top-5'>Bikes</div>
+            <div>{props.children}</div>
+        </div>
+    )
+}
+
+export default PerfilLayout
