ad3ff2b68519ad67782c45cb28aa29a1d175cddc
Ruan-Azeredo <ruanazeredo@gmail.com>
2024-01-02 12:03:41 -0300

    :bug: fix: impor missing dependencies

--- src/app/sistema/lojista/anuncios/page.jsx
+++ src/app/sistema/lojista/anuncios/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 import ClassificadosScreen from '@/components/sistema/screens/ClassificadosScreen'
+import { AuthContext } from '@/contexts/Auth'
 import React, { useContext } from 'react'
 
 const Anuncios = () => {
